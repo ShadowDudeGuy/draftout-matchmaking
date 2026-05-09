3ece1c8fee1e36fca69d15a2f1fef6cb9e84dba9
Shadow <makbasic2@gmail.com>
2026-05-09 22:12:50 +0200

    first matchmaking server

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let queue = [];
+
+app.post("/join", (req, res) => {
+    let name = req.body.name;
+
+    if (queue.length > 0) {
+        let opponent = queue.shift();
+
+        return res.json({
+            match: true,
+            opponent
+        });
+    }
+
+    queue.push(name);
+
+    res.json({
+        match: false
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("running on port " + PORT);
+});
